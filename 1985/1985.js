e39b1fe8ff775424efaf224b870cd7959e057db8
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2024-08-14 08:37:01 -0300

    Add 1980 files

--- 1985/1985.js
+++ 1985/1985.js
@@ -4,7 +4,6 @@ var files = [ "COMMAND.COM", "FORMAT.COM", "CHKDSK.COM", "SYS.COM", "DISKCOPY.CO
     eita.forEach(element => {
       document.getElementById("files").insertAdjacentHTML("beforeend", `<div class='item'>${element}</div>`);
     });
-    console.log("A:WIN101/");
     const ok = document.getElementsByClassName("item");
     var bruh = Array.from(ok);
     var lastSelected, lastSelectedClass;
@@ -20,11 +19,38 @@ var files = [ "COMMAND.COM", "FORMAT.COM", "CHKDSK.COM", "SYS.COM", "DISKCOPY.CO
             bruh.forEach(element => {
               element.remove();
             });
-            document.getElementById("files").insertAdjacentHTML("beforeend", `<div class='item'>IAI.KKK</div>`);
-            
+            openFolder();
           } else {
             alert("Cannot run " + element.innerHTML);
           }
         }
       }
     });
+
+    function openFolder() {
+      document.getElementById("files").insertAdjacentHTML("beforeend", `<div class='item'>DESIGN.TXT</div><div class='item'>HISTORY.TXT</div>`);
+      const tmj = document.getElementsByClassName("item");
+      var kkkiai = Array.from(tmj);
+      kkkiai.forEach(element => {
+        element.onclick = event => {
+        if (event.detail === 1) {
+          if (lastSelected) lastSelected.setAttribute("class", lastSelectedClass);
+          lastSelected = element;
+          lastSelectedClass = element.getAttribute("class");
+          element.setAttribute("class", "selected " + lastSelectedClass);
+        } else if (event.detail === 2) {
+          openFile(element.innerHTML);
+        }
+      }
+      });
+    }
+  
+    function openFile(file) {
+      document.getElementById("bar-title").innerHTML = "Notepad - " + file;
+      document.getElementById("view").innerHTML = "Edit";
+      document.getElementById("special").innerHTML = "Search";
+      document.getElementById("directorybar").remove();
+      document.getElementById("files").remove();
+      document.getElementById("main").remove();
+      document.getElementById("baragain").insertAdjacentHTML("afterend", "<textarea id='notepad' class='main' spellcheck='false' autofocus></textarea>");
+    }
