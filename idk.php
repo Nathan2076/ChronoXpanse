1af62c1150c5abc1700a4efbbdd87377bf888a06
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2024-10-05 17:35:16 -0300

    before editing directly through github

--- idk.php
+++ idk.php
@@ -8,7 +8,7 @@ $gitadd  = false;
 
 switch ($command[0]) {
     case "git":
-        git($command, $level, $gitadd);
+        git($command);
         break;
     case "timeline": case "tl": case "linhadotempo": case "ldt": case "lt":
         printTimeline($level);
@@ -17,22 +17,44 @@ switch ($command[0]) {
         echo "Comando ou nome de arquivo incorreto<br><br>";
 }
 
-function git($command, $level, $gitadd) {
+function git($command) {
+    global $gitadd, $level;
+    
     if (count($command) > 1 && $command[1] == "clone") {
         if (count($command) > 2 && $command[2] == "windows") {
             echo("booting");
-        } elseif (count($command) < 3) {
+        }
+        elseif (count($command) < 3) {
             echo "fatal: Você deve especificar um repositório para clonar.<br><br>";
-        } elseif ($command[2] != "windows") {
+        }
+        elseif ($command[2] != "windows") {
             echo "fatal: repositório '" . format($command[2]) . "' não existe";
         }
-    } elseif (count($command) > 1 && $command[1] == "add" && $level == 1985) {
+    }
+    elseif (count($command) > 1 && $command[1] == "add" && $level == 1985) {
         $gitadd = true;
-    } elseif (count($command) > 1 && $command[1] == "commit" && $gitadd && $level == 1985) {
-        echo "[main f3b3714] "
-    } elseif (count($command) < 2) {
+    }
+    elseif (count($command) > 1 && $command[1] == "commit" && $level == 1985) {
+        if (count($command) > 2 && $command[2] == "-m") {
+            if (count($command) > 3) {
+                $commit = "";
+                for ($i = 3; $i < count($command); $i++) {
+                    $commit .= $command[$i] . " ";
+                }
+                echo "[main f3b3714] " . format($commit) . "<br>&nbsp;2 arquivos alterados, 133 inserções(+), 7 exclusões(-)";
+            }
+            else {
+                echo "erro: a opção `m' requer um valor";
+            }
+        }
+        elseif (count($command) < 3) {
+            echo "Abortando commit devido a uma mensagem de commit vazia.";
+        }
+    }
+    elseif (count($command) < 2) {
         echo "uso: git &lt;comando&gt; [&lt;args&gt;]";
-    } elseif ($command[1] != "clone") {
+    }
+    elseif ($command[1] != "clone") {
         echo "git: '" . format($command[1]) . "' não é um comando git.";
     }
 }
