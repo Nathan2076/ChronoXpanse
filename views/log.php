333a192e5e8d65532473e58221f7136f0464347e
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2024-10-09 12:40:54 -0300

    corrigindo link css

--- views/log.php
+++ views/log.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" type="text/css" media="screen" href="../cadlog/cadlog.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="views/cadlog.css">
     <script src="log.js"></script>
 </head>
 
