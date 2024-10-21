a1314006c092ff5abe9a1b5a062774e0fac99ea5
julia-oliveira00 <almeidajuliaoliveira00@gmail.com>
2024-10-21 09:01:54 -0300

    arrumando caminho da logo

--- views/log.php
+++ views/log.php
@@ -13,7 +13,7 @@
 
 <body>
     <header>
-        <img alt="Logo" class="logo" src="../cadlog/logo.png">
+        <img alt="Logo" class="logo" src="views/cadlog/logo.png">
         <div class="info">
             <a href=".../index.html">início:)</a>
         </div>
