347df4712104a618deb9ed7cd3d0a04d999fab06
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2024-10-27 01:07:48 -0300

    Start 1995

--- 1995/1995.js
+++ 1995/1995.js
@@ -1,3 +1,28 @@
+let startup = new Audio("assets/startup.m4a");
+
 setTimeout(() => {
     document.getElementById("body").removeChild(document.getElementById("boot"));
+    startup.play();
 }, 4000);
+
+const ok = document.getElementsByClassName("item");
+    var bruh = Array.from(ok);
+    var lastSelected, lastSelectedClass;
+    bruh.forEach(element => {
+      element.onclick = event => {
+        if (event.detail === 1) {
+          if (lastSelected) lastSelected.setAttribute("class", lastSelectedClass);
+          lastSelected = element;
+          lastSelectedClass = element.getAttribute("class");
+          element.setAttribute("class", "selected " + lastSelectedClass);
+        } else if (event.detail === 2) {
+          if (element.lastElementChild.innerHTML == "Netscape Navigator") {
+            openNetscape();
+          }
+        }
+      }
+    });
+
+function openNetscape() {
+    document.getElementById("main").insertAdjacentHTML("beforeend", "<div id='netscape'>NETSACPE!!!!</div>")
+}
